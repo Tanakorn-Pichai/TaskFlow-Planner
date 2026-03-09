e93dd5957c0b600a798ccdecf6a227578dd51bf1
Kamonmat67 <6706022510328@email.kmutnb.ac.th>
2026-03-09 12:54:29 +0700

    remove backend files from repository

--- apiRoutes/reportRoutes.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router()
-const reportController = require('../apiControllers/reportController')
-
-router.get('/longest-tasks', reportController.longestTasks)
-router.get('/user-performance', reportController.userPerformance)
-
-module.exports = router
